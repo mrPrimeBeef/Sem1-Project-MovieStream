33dbea1f2049fb42a87a46b83ef0b8d9a359640d
Lasse Jensen <Lasse.Erik.Jensen@mensa.dk>
2024-04-18 11:19:04 +0200

    Added FileIO and Search classes, and some code in FileIO

---
+++ src/FileIO.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileIO extends Main{
+
+
+    public FileIO() {
+
+    }
+
+
+    public ArrayList<String> readMovieData(String moviePath){
+        ArrayList<String> movies = new ArrayList<>();
+        movies = scanFile(moviePath);
+
+        return movies;
+    }
+
+
+    public ArrayList<String> readSerieData(String seriePath){
+        ArrayList<String> series = new ArrayList<>();
+        series = scanFile(seriePath);
+
+        return series;
+    }
+
+
+    public void saveFavorites(ArrayList<User> user, String path){
+
+
+    }
+
+
+    public void saveWatched(ArrayList<User> user, String path){
+
+
+    }
+
+
+    private ArrayList<String> scanFile(String path){
+        ArrayList<String> list = new ArrayList<>();
+
+        try {
+            File file = new File(path);
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                list.add(scanner.next());
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        return list;
+    }
+}
