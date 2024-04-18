e7b951cda15939f0bf2832ce0fad3c09076e4fa4
idrissalvatoreisci <158306705+idrissalvatoreisci@users.noreply.github.com>
2024-04-18 11:16:15 +0200

    opdatering af classdiagram

---
+++ src/Episode.java
@@ -0,0 +1,3 @@
+public class Episode extends Series {
+
+}
---
+++ src/Series.java
@@ -0,0 +1,17 @@
+public class Series extends AMedia{
+    private int episode;
+    private int season;
+
+    @Override
+    public void playMedia() {
+
+    }
+
+    @Override
+    public void backToMenu(String q) {
+
+    }
+
+
+
+}
