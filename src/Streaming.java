01794d25e118342420d827cef4502ebe0cd1e500
malouVich <malouvichlavrentiew@gmail.com>
2024-04-18 11:13:02 +0200

    Stream

---
+++ src/Streaming.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Streaming {
+
+    private String name;
+
+    private User user;
+    private TextUI ui;
+    FileIO io;
+    Search search;
+    String moviePath;
+    String seriePath;
+    AMedia media;
+    ArrayList<Media> watchedList;
+    ArrayList<Media> SavedList;
+
+    public Streaming(String name) {
+        this.name = name;
+
+        this.ui = ui;
+        this.io = io;
+    }
+
+    public void runStreaming() {
+
+    }
+
+    public void Menu(){
+
+    }
+
+}
