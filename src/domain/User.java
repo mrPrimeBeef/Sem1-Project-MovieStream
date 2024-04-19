44b173f1e42c59518f8b4335ea81514dbf31b5aa
malouVich <malouvichlavrentiew@gmail.com>
2024-04-19 13:33:03 +0200

    user

--- src/domain/User.java
+++ src/domain/User.java
@@ -3,12 +3,13 @@ package domain;
 import application.AMedia;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class User {
     private String username;
     private String password;
-    ArrayList<AMedia> watchedList;
-    ArrayList<AMedia> SavedList;
+    LinkedList<AMedia> watchedList;
+    LinkedList<AMedia> savedList;
 
     public User(String username, String password) {
         this.username = username;
@@ -21,5 +22,32 @@ public class User {
     public String getPassword() {
         return password;
     }
+
+    public LinkedList<AMedia> getSavedList() {
+        return savedList;
+    }
+
+    public LinkedList<AMedia> getWatchedList() {
+        return watchedList;
+    }
+
+
+    public void addSavedMedia(AMedia media){
+        if(!savedList.contains(media)) {
+            savedList.add(media);
+        }
+    }
+    public void removeSavedMedia(AMedia media){
+        if(savedList.contains(media)){
+            savedList.remove(media);
+        }
+    }
+    public void addWatchedMedia(AMedia media) {
+        if (!watchedList.contains(media)){
+            watchedList.add(media);
+        }
+    }
+
+
 
 }
