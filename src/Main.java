9235d9648329dce380590912ed1d7d5b2ebcab71
Rolf <158441656+mrPrimeBeef@users.noreply.github.com>
2024-04-19 12:59:29 +0200

    start på create user

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
+    public Main() {
+    }
+
     public static void main(String[] args) {
-    Streaming streaming = new Streaming("Streamning'n'Dreamning");
-    streaming.runStreaming();
+        Streaming streaming = new Streaming("Streamning'n'Dreamning");
+        streaming.startStreaming();
     }
 }
--- src/Streaming.java
+++ src/Streaming.java
@@ -1,16 +1,17 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 import application.AMedia;
+import domain.User;
+import java.util.ArrayList;
 import utility.FileIO;
 import utility.Search;
 import utility.TextUI;
 
-import domain.User;
-
-import java.util.ArrayList;
-
 public class Streaming {
-
     private String name;
-
     private User user;
     private TextUI ui;
     FileIO io;
@@ -19,28 +20,69 @@ public class Streaming {
     String seriePath;
     AMedia media;
     ArrayList<User> userList;
+    ArrayList<String> startmenu;
 
-public Streaming(String name) {
-    this.name = name;
+    public Streaming(String name) {
+        this.name = name;
+        this.userList = new ArrayList();
+        this.name = name;
+        this.ui = new TextUI();
+        this.io = new FileIO();
+        this.startmenu = new ArrayList();
+        this.startmenu.add("1) Create user");
+        this.startmenu.add("2) Login");
+    }
 
-    userList = new ArrayList();
-}
+    public void startStreaming() {
+        this.ui.displayMessage("Welcome to " + this.name);
+        int action = 0;
 
-public void createUser(String username, String password) {
-    user = new User(username, password);
-    this.userList.add(user);
+        while(action != this.startmenu.size()) {
+            action = this.ui.promptChoice(this.startmenu, "Create a user or login:");
+            switch (action) {
+                case 1:
+                    this.createUser();
+                    this.runStreaming();
+                    break;
+                case 2:
+                    this.login();
+                    this.runStreaming();
+            }
+        }
 
-}
+    }
 
     public void runStreaming() {
-
-
     }
 
-    public void Menu(){
+    public User createUser() {
+        String username = this.ui.promptText("Please enter your username");
+        String password = this.ui.promptText("Please enter your password");
+        if (this.checkUsernameAvailability(username)) {
+            this.user = new User(username, password);
+            this.userList.add(this.user);
+        }
 
+        return this.user;
     }
 
+    boolean login() {
+        String username = this.ui.promptText("Please enter your username");
+        if (this.userList.contains(username)) {
+            return true;
+        } else {
+            this.ui.displayMessage("Wrong username or password");
+            return false;
+        }
+    }
 
-
+    boolean checkUsernameAvailability(String username) {
+        if (!this.userList.contains(username)) {
+            this.ui.displayMessage(username + " is available");
+            return true;
+        } else {
+            this.ui.displayMessage(username + "user exists... ");
+            return false;
+        }
+    }
 }
